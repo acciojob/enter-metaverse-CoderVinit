a1cfc4df297f3b4738e7805a1db60c05036e3896
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-26 11:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let div = document.getElementById("main");
+let button = document.getElementById("btn");
+
+button.addEventListener('click',()=>{
+	div.innerHTML = `<h1>Entered Metaverse<h1>`
+})
